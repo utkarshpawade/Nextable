cda4901e32b9de776aa28f0575c53fd0ccbb317d
Utkarsh <utkarshpawadecse@gmail.com>
2026-05-13 16:12:29 +0530

    bugfixes

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -3,7 +3,9 @@ import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import { codeAgentFunction } from "@/inngest/functions";
 
-// Create an API that serves zero functions
+// Each Inngest step is one Vercel invocation; default timeouts kill sandbox boot / npm install.
+export const maxDuration = 60;
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
